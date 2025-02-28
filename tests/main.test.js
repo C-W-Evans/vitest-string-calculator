96c74980dc5cd8368de86c4015f3237255ef0531
C-W-Evans <164054958+C-W-Evans@users.noreply.github.com>
2025-02-28 09:53:43 +0100

    test workflow

--- tests/main.test.js
+++ tests/main.test.js
@@ -39,6 +39,6 @@ describe('test the funtion add'), () => {
   })
   
   it('returns 8 for "1\n"', () => {
-    expect(() => add('-2, -4')).toThrowError('negatives not allowed. [-2, -4]')
+    expect(() => add('-2, -4')).toThrowError('negatives not allowed. [-2, -4]')q
   });
 }
